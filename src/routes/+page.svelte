11874735d95ebb8cc498aa82c144b30e65b7b06c
DefinitelyMaybe <DefinitelyMaybe@users.noreply.github.com>
2022-11-29 12:05:15 +1300

    moved inventory to different route. trying to render multiple elements.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,8 @@
 <script>
-	import { Canvas } from '@threlte/core';
-	import Scene from '$lib/Scene1.svelte';
-	import { World, Debug } from '@threlte/rapier';
-	import Inventory from '$lib/Inventory.svelte';
 	import '../app.css';
 </script>
 
-<div class="w-full h-full">
-	<Canvas>
-		<World>
-			<Debug />
-			<Scene />
-		</World>
-	</Canvas>
-</div>
-<div class="absolute z-10 top-0 w-full h-full pointer-events-none">
-	<div class="relative w-full h-full">
-		<Inventory />
-	</div>
-</div>
+<nav class="flex flex-col p-4">
+	<a href="/inventory">inventory</a>
+	<a href="/multipleElements">multiple Elements</a>
+</nav>
